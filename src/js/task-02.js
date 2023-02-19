0be340278f2c5dd6075b7c21812f4a1f63ae9a47
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-02-19 19:52:28 +0200

    complete second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (const ingredient of ingredients){
+let list = document.querySelector('#ingredients')
+ const element = document.createElement("li")
+element.classList.add("item")
+ element.textContent = ingredient;
+ list.append(element)
+}
