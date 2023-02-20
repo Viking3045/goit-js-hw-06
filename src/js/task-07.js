d9004c44d31dac8102fd759416e985624d26783d
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-02-20 15:36:03 +0200

    complete 7 task

---
+++ src/js/task-07.js
@@ -0,0 +1,13 @@
+let input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+span.style.fontSize = `${input.value}px`;
+const callback = function () {
+    span.style.fontSize = `${input.value}px`;
+}
+input.addEventListener('change', callback)
+
+
+   
+
+
+
