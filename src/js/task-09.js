ee5c68e6c05dcc355803ace4112b0b8c98706da1
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-02-20 17:40:58 +0200

    complete 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return   `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ 
+}
+const button = document.querySelector(".change-color")
+const color = document.querySelector(".color")
+const body = document.body
+button.addEventListener("click", bodyColor)
+function bodyColor (){
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  color.textContent = getRandomHexColor()
 }
