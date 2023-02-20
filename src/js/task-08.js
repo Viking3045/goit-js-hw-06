7e7a223b96d542a961e88cc43ebd7c849275881d
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-02-20 17:10:44 +0200

    complete task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form")
+
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!");
+    }
+else {
+        const formElements = event.currentTarget.elements;
+        const mail = formElements.email.value;
+        const password = formElements.password.value;
+        const formData = {
+    mail, 
+    password,
+}
+    console.log(formData)
+     form.reset()
+    }
+}
 
