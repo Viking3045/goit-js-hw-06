3da58635342b7eabaf4c0ec443c1b513a091d9b9
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-02-21 09:59:58 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,12 @@
-const allCategories = document.querySelectorAll(".item").length;
-console.log(`Number of categories: ${allCategories}`);
 
-const animals = document.querySelector("#categories").firstElementChild;
-const titleAnimals = animals.firstElementChild.textContent;
-const animalsLength = animals.children[1].children.length;
-console.log(`Category: ${titleAnimals} Elements: ${animalsLength}`);
+const categoriesList = document.querySelector("ul#categories");
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+console.log(`Number of categories: ${categoriesItems.length}`);
 
-const products = document.querySelector("#categories").children[1];
-const titleProducts = products.firstElementChild.textContent;
-const productsLength = products.children[1].children.length;
-console.log(`Category: ${titleProducts} Elements: ${productsLength}`);
 
-const technologies = document.querySelector("#categories").children[2];
-const titleTechnologies = technologies.firstElementChild.textContent;
-const technologiesLength = technologies.children[1].children.length;
-console.log(`Category: ${titleTechnologies} Elements: ${technologiesLength}`);
+categoriesItems.forEach(item =>{
+      const categoriesName = item.querySelector("h2").textContent;
+  const categoriesElements = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoriesName}`);
+  console.log(`Elements: ${categoriesElements}`);
+})
